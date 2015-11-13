7dac68e905467b04e0e460762c94f034e9c78764
Frederico da Silva Pereira <frederico.silva.pereira@gmail.com>
2015-11-13 21:27:36 -0200

    shortestPath removed, added closeness algoritm

--- closeness-core/src/scala/closeness/core/vertex/Graph.scala
+++ closeness-core/src/scala/closeness/core/vertex/Graph.scala
@@ -3,55 +3,57 @@ package closeness.core.vertex
 
 import scala.collection.immutable.BitSet
 import scala.annotation.tailrec
+import scala.collection.immutable.Set
 
 //There isnt any node alone...
 class Graph[V <: Number](nodes: Set[Node[V]], edges: Set[Edge[V]]) {
 
-  def add(v1: V, v2: V): Graph[V] = {
-    val n2 = Node(v2)
-    val n1 = Node(v1)
-    new Graph(nodes + (n1, n2), edges + Edge(n1, n2));
+  def add(in: V, out: V): Graph[V] = {
+    val nodeIn = Node(out)
+    val nodeOut = Node(in)
+    new Graph(nodes + (nodeIn, nodeOut), edges + Edge(nodeIn, nodeOut));
   }
+  lazy val closenessCentrality = new ClosenessCentrality(edges)
 
-  def closeness(v1: V): Float = {
-    val n1 = Node(v1)
-    1
+  def closeness(nodeId: V): Float = {
+    //http://objdig.ufrj.br/60/teses/coppe_m/LeandroQuintanilhaDeFreitas.pdf
+    1 / closenessCentrality.from(nodeId)
   }
 }
 
-protected class ShortestPath[T](edges: Set[Edge[T]]) {
+protected class ClosenessCentrality[T](edges: Set[Edge[T]]) {
 
-  lazy val outgoingEdges = edges.groupBy(_.v1)mapValues(s => s.map(_.v2))
-  lazy val incomingEdges = edges.groupBy(_.v2)mapValues(s => s.map(_.v1))
-  lazy val nodesLinkedOtherNodes: Map[Node[T], Set[Node[T]]] = outgoingEdges ++ incomingEdges.map{ 
-    case (k,v) => 
-      k -> (outgoingEdges.getOrElse(k, Set.empty) ++ v) 
-  }
-  
-  def from(origin: T): ShortestPathCalculator = {
-    val originNode = Node(origin)
-    new ShortestPathCalculator(originNode)
+  lazy val outgoingEdges = edges.groupBy(_.nodeIn) mapValues (s => s.map(_.nodeOut))
+  lazy val incomingEdges = edges.groupBy(_.nodeOut) mapValues (s => s.map(_.nodeIn))
+  lazy val nodesLinkedOtherNodes: Map[Node[T], Set[Node[T]]] = (outgoingEdges ++ incomingEdges).map {
+    case (k, v) =>
+      k -> (outgoingEdges.getOrElse(k, Set.empty) ++ v)
   }
 
-  class ShortestPathCalculator(originNode: Node[T]) {
-    def to(to: T): Integer = {
-      discover(originNode, Node(to), Set.empty)
+  def from(origin: T): Int = {
+    
+    def discover(from: Node[T], deepLevel: Int, edgesAlreadyPassed: Set[Node[T]]): Int = {
+      val edgesNotVerified: List[Node[T]] = nodesLinkedOtherNodes getOrElse (from, Set()) diff edgesAlreadyPassed toList;
+      if (edgesNotVerified.isEmpty) 0
+      else {
+        val total = edgesNotVerified
+          .view
+          .foldLeft(0) {
+            case (acc, value) => acc + discover(value, deepLevel + 1, edgesAlreadyPassed ++ edgesNotVerified)
+          }
+        (edgesNotVerified.size * deepLevel) + total
+      }
     }
     
-    private def discover(origin: Node[T], to: Node[T], edgesAlreadyPassed: Set[Node[T]]): Integer = {
-      val originEdges = nodesLinkedOtherNodes.get(origin)
-      val edgesNotVerified: Option[Set[Node[T]]] = originEdges.map { x => x diff edgesAlreadyPassed }
-      if (edgesNotVerified.isEmpty) 0
-      if (edgesNotVerified.get.contains(to)) 1
-      else edgesNotVerified.get.toList.map { x => discover(x, to, edgesAlreadyPassed+x) }.foldLeft(0)(_+_)
-    }
+    val originNode = Node(origin)
+    discover(originNode, 1, Set(originNode))
   }
 
 }
 
 case class Node[+T](value: T)
-case class Edge[+T](v1: Node[T], v2: Node[T]) {
-  def this(v1: T, v2: T) {
-    this(Node(v1), Node(v2))
+case class Edge[+T](nodeIn: Node[T], nodeOut: Node[T]) {
+  def this(in: T, out: T) {
+    this(Node(in), Node(out))
   }
 }
